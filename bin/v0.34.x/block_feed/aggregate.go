3b0f6c2cf3b31b87f0240d813c42498de641acf0
Jesse Chung <jesse@soob.co>
2021-10-18 02:53:30 +0900

    chore: add debugging message for ws switch

--- bin/v0.34.x/block_feed/aggregate.go
+++ bin/v0.34.x/block_feed/aggregate.go
@@ -68,7 +68,7 @@ func (ags *AggregateSubscription) Subscribe(rpcIndex int) (chan *BlockResult, er
 	if firstBlock := <-cWS; firstBlock.Block.Header.Height != ags.lastKnownBlock+1 {
 		log.Printf("[block_feed/aggregate] received the first block(%d), but local blockchain is at (%d)\n", firstBlock.Block.Header.Height, ags.lastKnownBlock)
 		go func() {
-			go ags.rpc.SyncFromUntil(ags.lastKnownBlock+1, firstBlock.Block.Header.Height+1, rpcIndex)
+			go ags.rpc.SyncFromUntil(ags.lastKnownBlock+1, firstBlock.Block.Header.Height, rpcIndex)
 			for {
 				r := <-cRpc
 				if r != done {
@@ -77,6 +77,8 @@ func (ags *AggregateSubscription) Subscribe(rpcIndex int) (chan *BlockResult, er
 					break
 				}
 			}
+
+			fmt.Println("[block_feed/aggregate] switching to ws...")
 
 			// patch ws to aggregate
 			for {
