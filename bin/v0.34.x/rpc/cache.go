e6654feccba79870b3fc250b76f4166ec95f8328
Jesse Chung <jesse@soob.co>
2021-11-05 21:27:32 +0900

    feat: querier cache

--- bin/v0.34.x/rpc/cache.go
+++ bin/v0.34.x/rpc/cache.go
@@ -5,6 +5,7 @@ import (
 	lru "github.com/hashicorp/golang-lru"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 )
 
 type ResponseCache struct {
@@ -13,8 +14,11 @@ type ResponseCache struct {
 }
 
 type CacheBackend struct {
-	lru           *lru.Cache
-	evictionCount uint64
+	lru             *lru.Cache
+	evictionCount   uint64
+	cacheServeCount uint64
+	serveCount      uint64
+	mtx             *sync.Mutex
 }
 
 func NewCacheBackend(cacheSize int) *CacheBackend {
@@ -27,6 +31,7 @@ func NewCacheBackend(cacheSize int) *CacheBackend {
 	return &CacheBackend{
 		lru:           cache,
 		evictionCount: 0,
+		mtx:           new(sync.Mutex),
 	}
 }
 
@@ -50,19 +55,36 @@ func (cb *CacheBackend) Get(cacheKey string) *ResponseCache {
 }
 
 func (cb *CacheBackend) Purge() int {
-	defer fmt.Printf("[rpc/cache] cache eviction count %d\n", cb.evictionCount)
+	fmt.Printf("[rpc/cache] cache eviction count %d, serveCount %d, cacheServeCount %d\n",
+		cb.evictionCount,
+		cb.serveCount,
+		cb.cacheServeCount,
+	)
+
+	cb.mtx.Lock()
 	cacheLen := cb.lru.Len()
 	cb.lru.Purge()
 	cb.evictionCount = 0
+	cb.cacheServeCount = 0
+	cb.serveCount = 0
+	cb.mtx.Unlock()
 	return cacheLen
 }
 
 func (cb *CacheBackend) HandleCachedHTTP(writer http.ResponseWriter, request *http.Request, handler http.Handler) {
+	cb.mtx.Lock()
+	cb.serveCount++
+	cb.mtx.Unlock()
+
 	cached := cb.Get(request.URL.String())
 	// if cached, return as is
 	if cached != nil {
 		writer.WriteHeader(cached.status)
 		writer.Write(cached.body)
+
+		cb.mtx.Lock()
+		cb.cacheServeCount++
+		cb.mtx.Unlock()
 		return
 	}
 
@@ -73,4 +95,8 @@ func (cb *CacheBackend) HandleCachedHTTP(writer http.ResponseWriter, request *ht
 
 	// set in cache
 	cb.Set(request.URL.String(), recorder.Code, recorder.Body.Bytes())
+
+	// write
+	writer.WriteHeader(recorder.Code)
+	writer.Write(recorder.Body.Bytes())
 }
